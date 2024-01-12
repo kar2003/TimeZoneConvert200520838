4cb277da56172a217d0caa7996ac25c3cff2b715
kar2003 <200520838@student.georgianc.on.ca>
2024-01-12 16:56:17 -0500

    main, fxml and added css

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,15 @@
 module com.example.timezoneconvert200520838 {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.kordamp.bootstrapfx.core;
+    requires async.http.client.netty.utils;
+    requires async.http.client;
+    requires com.fasterxml.jackson.databind;
+    requires json.simple;
 
     opens com.example.timezoneconvert200520838 to javafx.fxml;
     exports com.example.timezoneconvert200520838;
+    exports com.example.timezoneconvert200520838.controller;
+    exports com.example.timezoneconvert200520838.model;
+    opens com.example.timezoneconvert200520838.controller to javafx.fxml;
 }
